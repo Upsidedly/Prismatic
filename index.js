c44146329e69b1066798bd87028c937f0c244c9c
Upsided <matthewlloydw@gmail.com>
2021-11-26 11:27:05 -0500

    yeh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({intents:["GUILDS","GUILD_MESSAGES"]});
-const token = require("./token.js");
+import { token } from './token'
 
 var prefix = "."
 var mention = "<@762659603543752704>"
--- token.js
+++ token.js
@@ -1,1 +1,1 @@
-token = 'Nzc3Mjc4MzU3OTEzMjA2ODA0.X7BG4Q.nfvZAsdbNbTsOKAfyNtmSZ2FswE'
+token = 'Nzc3Mjc4MzU3OTEzMjA2ODA0.X7BG4Q.DfLuQxLXSejzKfuj0w9F8-akQeY'
